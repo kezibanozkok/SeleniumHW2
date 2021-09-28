1b5a46addfa80a0c35a0f8df02df0d283eb597e6
sermis97 <sermis97@gmail.com>
2021-09-28 20:16:40 +0300

    Test Case started

--- src/test/java/com/cybertek/tests/Q1.java
+++ src/test/java/com/cybertek/tests/Q1.java
@@ -14,41 +14,11 @@ public class Q1 {
     /*
 
     Test case #1
-    Step 1. Go to “https://practice-cybertekschool.herokuapp.com”
+    Step 1. Go to “https://practice- cybertekschool.herokuapp.com”
     Step 2. Click on “Registration Form”
     Step 3. Enter “wrong_dob” into date of birth input box.
     Step 4. Verify that warning message is displayed: “The date of birth is not valid”
      */
-    WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-
-    @Test
-    public void test1() {
-        //Step 1. Go to “https://practice-cybertekschool.herokuapp.com”
-        driver.get("https://practice-cybertekschool.herokuapp.com");
-
-        //Step 2. Click on “Registration Form”
-        WebElement registrationFormLink = driver.findElement(By.xpath("//a[.= 'Registration Form']"));
-        registrationFormLink.click();
-
-        //Step 3. Enter “wrong_dob” into date of birth input box.
-        WebElement dateOfBirth = driver.findElement(By.name("birthday"));
-        dateOfBirth.sendKeys("wrong_dob");
-
-        WebElement errorMessage = driver.findElement(By.xpath("//input[@name='birthday']/following-sibling::small[2]"));
-        String actualMessage = errorMessage.getText();
-
-        Assert.assertEquals(actualMessage, "The date of birth is not valid", "Verify failed");
-
-    }
+    
 }
